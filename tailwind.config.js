ad821c75b4667829d2468c6c63e0de85a31143f3
vadim <vadim444@hotmail.com>
2022-02-10 20:21:11 +0000

    added sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false, //or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '547px',
+      // => @media (min-width: 547px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      '2xl': '1680px',
+      // => @media (min-width: 1680px) { ... }
+    },
     extend: {
       colors: {
         primary: "#03045e",
