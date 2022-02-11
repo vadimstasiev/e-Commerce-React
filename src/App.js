86835dc0b76a1c7403c4da130f4fe7b1306b5b8a
vadimstasiev <vadim444@hotmail.com>
2022-02-11 17:19:58 +0000

    added dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {ThemeProvider} from './Components/ThemeContext';
+import Background from './Components/Background';
+import Toggle from './Components/ThemeToggle';
+
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        {/* <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/recovery-password" element={<RecoveryPassword/>}/> */}
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Background>
+        <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+          <Toggle />
+        </div>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            {/* <Route exact path="/login" element={<Login/>}/>
+            <Route exact path="/recovery-password" element={<RecoveryPassword/>}/> */}
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </Router>
+      </Background>
+    </ThemeProvider>
   )
 }
 
