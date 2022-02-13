d2c20bda32ee3d138c7221d60d4ac6d5762fd1a2
vadimstasiev <vadim444@hotmail.com>
2022-02-13 20:38:36 +0000

    sorted some issues and added log in with google

--- src/pages/Auth/SignIn.js
+++ src/pages/Auth/SignIn.js
@@ -10,86 +10,75 @@ import { auth, signInWithGoogle } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const SignIn = (props) => {
+    const {from} = props
     const navigate = useNavigate()
     const [user, loading, error] = useAuthState(auth);
-
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
     const [authError, setAuthError] = useState("");
-    
-
 
     const handleSubmit = e => {
-        e.preventDefault();
-        console.log({ email, password })
-        signInWithEmailAndPassword(email, password)
+        signInWithEmailAndPassword(auth, email, password)
         .then(creds => {
             console.log(creds)
             setAuthError("")
         })
         .catch(error => {
-            let errorMessageFormated = error.code.replace('auth/','').replace(/-/g, " ")
-            errorMessageFormated = errorMessageFormated.charAt(0).toUpperCase() + errorMessageFormated.slice(1) + "."
-            setAuthError(errorMessageFormated)
+            if(error.code){
+              let errorMessageFormated = error.code.replace('auth/','').replace(/-/g, " ")
+              errorMessageFormated = errorMessageFormated.charAt(0).toUpperCase() + errorMessageFormated.slice(1) + "."
+              setAuthError(errorMessageFormated)
+            } else {
+              setAuthError("Unkown Error")
+            }
         })
     };
 
 
     useEffect(() => {
-      if (user) navigate("/home");
+      if (user) navigate(from?from:"/");
     }, [user, loading]);
 
-    let from = { pathname: "/" }
-
-    // if (auth.uid) return <Navigate to={from} />;
     return (
       <SvgBackground>
         <Toggle />
-          {/* <div className="social-login">
-            <button
-              className="ml-3 btn btn-outline-danger"
-              type="button"
-              onClick={logInWithGoogle}
-            >
-              <span>
-                <i className="fab fa-google-plus-g" /> Sign in with Google
-              </span>
-            </button>
-          </div>
-          <p> OR </p> */}
-
         <div className="grid place-items-center pt-40">
             <form className="bg-white dark:bg-zinc-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 border-zinc-200 border-2">
                 <div className="mb-4">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Email
-                </label>
-                <input autoComplete="email" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
-                id="email" type="text" placeholder="Email"
-                onChange={(e)=>setEmail(e.target.value)}
-                />
+                  <button className="w-full bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+                    onClick={signInWithGoogle}>
+                        Login with Google
+                  </button>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Email
+                  </label>
+                  <input autoComplete="email" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                  id="email" type="text" placeholder="Email"
+                  onChange={(e)=>setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="mb-6">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Password
-                </label>
-                <input autoComplete="current-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password" type="password" placeholder="******************"
-                onChange={(e)=>setPassword(e.target.value)}/>
-                {authError ? 
-                    <p className="text-red-500 text-xs italic">{authError}</p>
-                : null}
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Password
+                  </label>
+                  <input autoComplete="current-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password" type="password" placeholder="******************"
+                  onChange={(e)=>setPassword(e.target.value)}/>
+                  {authError ? 
+                      <p className="text-red-500 text-xs italic">{authError}</p>
+                  : null}
                 </div>
                 <div className="flex items-center justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-                onClick={handleSubmit}>
-                    Sign In
-                </button>
-                <div className="inline-block align-baseline font-bold text-sm ml-20 text-blue-500">
-                    <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/ResetPassword')}>Forgot Password?</div>
-                    <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/SignUp')}>Don't have an account? Sign Up!</div>
-                </div>
+                  <button className="bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+                    onClick={handleSubmit}>
+                      Sign In
+                  </button>
+                  <div className="inline-block align-baseline font-bold text-sm ml-20 text-blue-500">
+                      <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/ResetPassword')}>Forgot Password?</div>
+                      <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/SignUp')}>Don't have an account? Sign Up!</div>
+                  </div>
                 </div>
             </form>
         </div>
--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -10,6 +10,7 @@ import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const SignUp = (props) => {
+    const {from} = props
     const navigate = useNavigate()
     const [user, loading, error] = useAuthState(auth);
 
@@ -25,9 +26,13 @@ const SignUp = (props) => {
       if(password===confirmPassword){
         registerWithEmailAndPassword(name, email, password)
         .catch(error => {
-          let errorMessageFormated = error.code.replace('auth/','').replace(/-/g, " ")
-          errorMessageFormated = errorMessageFormated.charAt(0).toUpperCase() + errorMessageFormated.slice(1) + "."
-          setAuthError(errorMessageFormated)
+          if(error.code){
+            let errorMessageFormated = error.code.replace('auth/','').replace(/-/g, " ")
+            errorMessageFormated = errorMessageFormated.charAt(0).toUpperCase() + errorMessageFormated.slice(1) + "."
+            setAuthError(errorMessageFormated)
+          } else {
+            setAuthError("Unkown Error")
+          }
         })
       } else {
         setAuthError("Passwords must match.")
@@ -35,85 +40,61 @@ const SignUp = (props) => {
     }
 
 
-
-    // const logInWithGoogle = () => {
-    //     firebase.login({
-    //         provider: 'google',
-    //         type: 'redirect'
-    //     })
-    // };
-
     useEffect(() => {
-      if (user) navigate("/home");
+      if (user) navigate(from?from:"/");
     }, [user, loading]);
 
-    let from = { pathname: "/" }
-
-    // if (auth.uid) return <Navigate to={from} />;
     return (
       <SvgBackground>
         <Toggle />
-          {/* <div className="social-login">
-            <button
-              className="ml-3 btn btn-outline-danger"
-              type="button"
-              onClick={logInWithGoogle}
-            >
-              <span>
-                <i className="fab fa-google-plus-g" /> Sign in with Google
-              </span>
-            </button>
-          </div>
-          <p> OR </p> */}
-
         <div className="grid place-items-center pt-40">
             <form className="bg-white dark:bg-zinc-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 border-zinc-200 border-2">
                 <div className="mb-4">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Name
-                </label>
-                <input autoComplete="name" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
-                id="name" type="text" placeholder="Email"
-                onChange={(e)=>setName(e.target.value)}
-                />
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Name
+                  </label>
+                  <input autoComplete="name" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                  id="name" type="text" placeholder="Email"
+                  onChange={(e)=>setName(e.target.value)}
+                  />
                 </div>
                 <div className="mb-4">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Email
-                </label>
-                <input autoComplete="email" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
-                id="email" type="text" placeholder="Email"
-                onChange={(e)=>setEmail(e.target.value)}
-                />
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Email
+                  </label>
+                  <input autoComplete="email" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                  id="email" type="text" placeholder="Email"
+                  onChange={(e)=>setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="mb-1">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Password
-                </label>
-                <input autoComplete="new-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password" type="password" placeholder="******************"
-                onChange={(e)=>setPassword(e.target.value)}/>
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Password
+                  </label>
+                  <input autoComplete="new-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password" type="password" placeholder="******************"
+                  onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div className="mb-6">
-                <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
-                    Confirm Password
-                </label>
-                <input autoComplete="new-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="confirm_password" type="password" placeholder={ password.length===0?"******************":"*".repeat(password.length)}
-                onChange={(e)=>setConfirmPassword(e.target.value)}/>
-                {authError ? 
-                    <p className="text-red-500 text-xs italic">{authError}</p>
-                : null}
+                  <label className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2">
+                      Confirm Password
+                  </label>
+                  <input autoComplete="new-password" className="shadow appearance-none border rounded w-full py-2 px-3 dark:bg-zinc-900 dark:border-zinc-500 text-gray-700 dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="confirm_password" type="password" placeholder={ password.length===0?"******************":"*".repeat(password.length)}
+                  onChange={(e)=>setConfirmPassword(e.target.value)}/>
+                  {authError ? 
+                      <p className="text-red-500 text-xs italic">{authError}</p>
+                  : null}
                 </div>
                 <div className="flex items-center justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-                onClick={handleSubmit}>
-                    Sign Up
-                </button>
-                <div className="inline-block align-baseline font-bold text-sm ml-20 text-blue-500">
-                <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/ResetPassword')}>Forgot Password?</div>
+                  <button className="bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+                    onClick={handleSubmit}>
+                        Sign Up
+                  </button>
+                  <div className="inline-block align-baseline font-bold text-sm ml-20 text-blue-500">
+                    <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/ResetPassword')}>Forgot Password?</div>
                     <div className="hover:text-blue-800 cursor-pointer" onClick={() => navigate('/SignIn')}>Already have an account? Sign In!</div>
-                </div>
+                  </div>
                 </div>
             </form>
         </div>
