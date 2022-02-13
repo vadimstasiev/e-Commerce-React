b2ed2aba10e19f9b12159b4f8aff55f7e73c722c
vadimstasiev <vadim444@hotmail.com>
2022-02-13 17:39:36 +0000

    forgot to add auth fetch from redux

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { isLoaded, isEmpty } from 'react-redux-firebase'
 
 const PrivateRoute = () => {
-    const auth = null; // determine if authorized, from context or however you're doing it
+    const auth = useSelector(state => state.firebase.auth)
 
     // If authorized, return an outlet that will render child elements
     // If not, return element that will navigate to login page
