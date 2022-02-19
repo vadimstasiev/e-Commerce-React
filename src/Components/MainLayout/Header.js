7da0dad0df8683954c4004e95fadc1d466635b18
vadimstasiev <vadim444@hotmail.com>
2022-02-19 22:30:57 +0000

    add item menu

--- src/Components/MainLayout/Header.js
+++ src/Components/MainLayout/Header.js
@@ -5,7 +5,72 @@ import { Popover, Transition, Menu } from '@headlessui/react'
 import { Toggle } from "../ThemeToggle";
 import { logout } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { AppstoreAddOutlined } from '@ant-design/icons';
 
+const AddMoreItemsIconMenu = props => {
+  const { children, customClass } = props 
+  const navigate = useNavigate()
+
+
+  return (
+    <Menu>
+          {({ open }) => (
+            <>
+              <Menu.Button>
+                <div className={customClass}>{children}</div>
+              </Menu.Button>
+              <Transition
+                show={open}
+                enter="transition ease-out duration-500"
+                enterFrom=" opacity-0 "
+                enterTo=" opacity-100 "
+                leave="transition ease-in duration-500"
+                leaveFrom=" opacity-100 "
+                leaveTo=" opacity-0 "
+              >
+                <div className="static">
+                  <Menu.Items
+                    static
+                    className="absolute right-[180px] w-40 mt-6 origin-top-right bg-zinc-300 dark:bg-zinc-900 border border-zinc-200  divide-zinc-100 rounded-md shadow-lg outline-none"
+                  >
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <div
+                            className={`cursor-pointer ${
+                              active
+                              ? "bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-200"
+                              : "text-zinc-700 dark:text-zinc-100"
+                            } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                            onClick={() => navigate('/SignIn')}
+                          >
+                            Publish an item
+                          </div>
+                        )}
+                      </Menu.Item>
+                      {/* <Menu.Item>
+                        {({ active }) => (
+                          <div
+                            className={`cursor-pointer ${
+                              active
+                              ? "bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-200"
+                              : "text-zinc-700 dark:text-zinc-100"
+                            } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                            onClick={() => navigate("/")}
+                          >
+                            Option 2
+                          </div>
+                        )}
+                      </Menu.Item> */}
+                    </div>
+                  </Menu.Items>
+                </div>
+              </Transition>
+            </>
+          )}
+        </Menu>
+  )
+}
 
 const ProfileIconMenu = props => {
   const { children, customClass } = props 
@@ -18,7 +83,7 @@ const ProfileIconMenu = props => {
           {({ open }) => (
             <>
               <Menu.Button>
-                <div>{children}</div>
+                <div className={customClass}>{children}</div>
               </Menu.Button>
               <Transition
                 show={open}
@@ -33,7 +98,7 @@ const ProfileIconMenu = props => {
                 {user?
                   <Menu.Items
                     static
-                    className="absolute right-0 w-52 mt-4 origin-top-right origin-top-right bg-zinc-300 dark:bg-zinc-900 border border-zinc-200 divide-y divide-zinc-100 rounded-md shadow-lg outline-none"
+                    className="absolute right-[85px] w-52 mt-6 origin-top-right bg-zinc-300 dark:bg-zinc-900 border border-zinc-200 divide-y divide-zinc-100 rounded-md shadow-lg outline-none"
                   >
                     <div className="px-4 py-3">
                       <p className="text-sm leading-5 text-zinc-900 dark:text-zinc-200">Signed in as</p>
@@ -77,7 +142,7 @@ const ProfileIconMenu = props => {
                   :
                   <Menu.Items
                     static
-                    className="absolute right-0 w-40 mt-4 origin-top-right origin-top-right bg-zinc-300 dark:bg-zinc-900 border border-zinc-200  divide-zinc-100 rounded-md shadow-lg outline-none"
+                    className="absolute right-[85px] w-[130px] mt-6 origin-top-right bg-zinc-300 dark:bg-zinc-900 border border-zinc-200 divide-y divide-zinc-100 rounded-md shadow-lg outline-none"
                   >
                     <div className="py-1">
                       <Menu.Item>
@@ -122,6 +187,7 @@ const ProfileIconMenu = props => {
 const Header = () => {
 
   const [isActive, setIsActive] = useState(false);
+  const [user, loadingUser, error] = useAuthState(auth);
 
 
   const activeSide = "bg-zinc-300 dark:bg-zinc-900 h-screen w-80 transform transition-all fixed duration-700 text-white flex justify-center p-2"
@@ -170,13 +236,50 @@ const Header = () => {
               </a>
             </div>
             <div className="relative order-2 md:order-3 flex items-center" id="nav-content">
-              <ProfileIconMenu customClass="pt-0.5 inline-block" >
-                <svg className="fill-current dark:text-zinc-200 hover:text-black dark:hover:text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                  <circle fill="none" cx="12" cy="7" r="3" />
-                  <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
-                </svg>
+              
+
+              <AddMoreItemsIconMenu customClass="mr-4 pt-0.5 inline-block" >
+                <span className="rounded-md shadow-sm">
+                  <div className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white dark:bg-zinc-900 border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
+                    <AppstoreAddOutlined className="pt-0.5 dark:text-zinc-200 hover:text-black dark:hover:text-white"/>
+
+                    <svg
+                      className="w-5 h-5 ml-2 -mr-1 fill-current dark:text-zinc-200 hover:text-black dark:hover:text-white"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </span>
+              </AddMoreItemsIconMenu>
+              
+              <ProfileIconMenu customClass="mr-4 pt-0.5 inline-block" >
+              <span className="rounded-md shadow-sm">
+                  <div className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white dark:bg-zinc-900 border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
+                    <svg className="fill-current dark:text-zinc-200 hover:text-black dark:hover:text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                      <circle fill="none" cx="12" cy="7" r="3" />
+                      <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
+                    </svg>
+                    <svg
+                      className="w-5 h-5 ml-2 -mr-1 fill-current dark:text-zinc-200 hover:text-black dark:hover:text-white"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+              </span>
               </ProfileIconMenu>
-              <a className="pl-3 inline-block" href="#">
+              <a className="pl-1 inline-block" href="#">
                 <svg className="fill-current dark:text-zinc-200 hover:text-black dark:hover:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
                   <circle cx="10.5" cy="18.5" r="1.5" />
