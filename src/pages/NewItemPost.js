619bfbf801b8158bbb4981a412b435d2c2a634f1
vadimstasiev <vadim444@hotmail.com>
2022-02-20 18:40:59 +0000

    fixed style

--- src/pages/NewItemPost.js
+++ src/pages/NewItemPost.js
@@ -133,8 +133,8 @@ const NewItemPost = () => {
                                         </div>
                                         :
                                         <div className='flex flex-col items-center justify-center py-4'>
-                                            <svg className="w-10 h-10 text-gray-400 group-hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                                            <p className='lowercase text-sm text-gray-400 group-hover:text-gray-600 pt-1 tracking-wider'>Select a photo</p>
+                                            <svg className="w-10 h-10 text-gray-400 group-hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                                            <p className='lowercase text-sm text-gray-400 group-hover:text-white pt-1 tracking-wider'>Select a photo</p>
                                         </div>
                                     }
                                     </div>
