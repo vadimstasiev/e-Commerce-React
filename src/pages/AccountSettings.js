c2859f84d014e22df7b92163d6f731bba0133210
vadimstasiev <vadim444@hotmail.com>
2022-02-27 18:33:12 +0000

    user accounts settings

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -1,83 +1,199 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Background from '../Components/Background';
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import SvgBackground from "../Components/SvgBackground";
 import {FixedToggle} from "../Components/ThemeToggle";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Modal, Button } from 'antd';
+import { SyncOutlined, CloseOutlined, CheckOutlined, ExclamationCircleOutlined  } from '@ant-design/icons';
+import PriceInput from '../Components/Input/PriceInput';
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+  } from "use-places-autocomplete";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+    doc, 
+    getDoc, 
+    setDoc, 
+    deleteDoc 
+} from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
 
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
-const AccountSettings = () => {
+import * as geofire from 'geofire-common';
+import withRouter from '../Components/hooks/withRouter';
+import Loading from './Auth/Loading';
+import DangerConfirm from '../Components/DangerConfirm';
+
+const storage = getStorage();
+
+const AccountSettings = (props) => {
     const navigate = useNavigate()
+    const [user, loadingUser, error] = useAuthState(auth);
+    const [name, setName] = useState("");
+    const [postcode, setPostcode] = useState("");
+    const [formattedAddress, setFormattedAddress] = useState("");
+    const [coordinates, setCoordinates] = useState({});
+    const [geoHash, setGeoHash] = useState("");
+    // none, loading, error, success
+    const [postcodeInputStatus, setPostcodeInputStatus] = useState("none");
+    // none, uploading, uploaded
+    const [uploadingImagesStatus, setUploadingImagesStatus] = useState("none");
 
+
+
+    const validatePostCode = postcode => {
+        setPostcodeInputStatus("loading")
+        getGeocode({ address:postcode })
+        .then((result)=>{
+            if(result){
+                setPostcode(postcode)
+                
+                const lat = result[0].geometry.location.lat()
+                const lng = result[0].geometry.location.lng()
+                setFormattedAddress(result[0].formatted_address)
+
+                setCoordinates({lat: result[0].geometry.location.lat(), lng: result[0].geometry.location.lng()})
+
+                // Compute the GeoHash for a lat/lng point
+                const hash = geofire.geohashForLocation([lat, lng]);
+
+                setGeoHash(hash)
+
+                setPostcodeInputStatus("success")
+            } else {
+                setPostcodeInputStatus("error")
+            }
+        })
+        .catch(e => {
+            setPostcodeInputStatus("error")
+        })
+    }
+
+    const submit = async () => {
+        if (
+            name.length>0 &&
+            postcodeInputStatus==="success"
+        ) {
+            
+            await setDoc(doc(db, "users", user.uid), {
+                userUid: user.uid,
+                name: name,
+                postcode,
+                coordinates,
+                geoHash,
+            })
+            .then(navigate("/"))
+            .catch(error => {
+                alert(error)
+            })
+        } 
+    }
+
+    // Edit existing item 
+
+    const fetchItem = async () => {
+        await getDoc(doc(db, "users", user.uid))
+        .then(data => {
+            const actualData = data.data()
+            if(actualData){
+                setName(actualData.name)
+                validatePostCode(actualData.postcode)
+            } else {
+                navigate("/Not-Found")
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+            navigate("/Not-Found")
+        })
+    }
+
+    useEffect(() => {
+        fetchItem()
+    }, []);
+
+    // useEffect(() => {
+    //   console.log(itemsSameSeller)    
+    //   console.log(item)    
+    // }, [itemsSameSeller, item]);
+
+    
     return (
         <Background>
         <SvgBackground>
-        <FixedToggle/>
-        <div className="flex h-screen items-center justify-center  ">
-            <div className="grid bg-white dark:bg-zinc-900 rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-1/2">
-                <div className="flex justify-center py-4">
-                <div className="flex bg-gray-200 dark:bg-gray-700 rounded-full md:p-4 p-2 border-2 border-gray-300">
-                    <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
-                </div>
-                </div>
+            <FixedToggle/>
+            
+            <div className="flex my-auto items-center justify-center">
+                <div className="grid bg-white dark:bg-zinc-900 rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-2/5">
+                    <div className="grid grid-cols-1 mt-5 mx-7">
+                        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Full Name</label>
+                        <input value={name} onChange={e => setName(e.target.value)} className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Please enter a descriptive title" />
+                    </div>
 
-                <div className="flex justify-center">
-                <div className="flex">
-                    <h1 className="text-gray-600 font-bold md:text-2xl text-xl">Tailwind Form</h1>
-                </div>
-                </div>
-
-                <div className="grid grid-cols-1 mt-5 mx-7">
-                <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
-                <input className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Input 1" />
-                </div>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
-                <div className="grid grid-cols-1">
-                    <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 2</label>
-                    <input className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Input 2" />
-                </div>
-                <div className="grid grid-cols-1">
-                    <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 3</label>
-                    <input className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Input 3" />
-                </div>
-                </div>
-
-                <div className="grid grid-cols-1 mt-5 mx-7">
-                <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Selection</label>
-                <select className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent">
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                </select>
-                </div>
-
-                <div className="grid grid-cols-1 mt-5 mx-7">
-                <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Another Input</label>
-                <input className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Another Input" />
-                </div>
-
-                <div className="grid grid-cols-1 mt-5 mx-7">
-                <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Upload Photo</label>
-                    <div className='flex items-center justify-center w-full'>
-                        <label className='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-gray-300 dark:bg-zinc-700 dark:hover:bg-zinc-900 group'>
-                            <div className='flex flex-col items-center justify-center pt-7'>
-                            <svg className="w-10 h-10 text-gray-400 group-hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                            <p className='lowercase text-sm text-gray-400 group-hover:text-gray-600 pt-1 tracking-wider'>Select a photo</p>
+                    <div className="grid grid-cols-1 gap-5 md:gap-8 mt-5 mx-7">
+                        <div className="grid grid-cols-1">
+                            <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Postcode (UK Only)</label>
+                            <input 
+                            defaultValue={postcode}
+                            onChange={e=>{
+                                const value = e.target.value
+                                let re_no_space = new RegExp(`^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$`)
+                                let re_with_space = new RegExp(`^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$`)
+                                if (re_no_space.test(value) || re_with_space.test(value)) {
+                                    validatePostCode(value)
+                                } else {
+                                    setPostcodeInputStatus("none")
+                                }
+                            }} className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Postcode" />
+                            
+                            <div>
+                                {
+                                    postcodeInputStatus==="none" && <div className='py-6'></div>
+                                }
+                                {
+                                    postcodeInputStatus==="loading" &&
+                                        <>
+                                            <SyncOutlined spin  className="inline-block dark:text-zinc-200 pb-[6px] text-xs text-light font-semibold mb-1 py-2 pl-2"/>
+                                            <div className='inline-block dark:text-zinc-200 hover:text-black dark:hover:text-white px-1 pt-2'>Validating Postcode...</div>
+                                        </>
+                                }
+                                {
+                                    postcodeInputStatus==="error" &&
+                                        <>
+                                            <CloseOutlined className="inline-block text-red-400 dark:text-red-600 pb-[6px] text-xs text-light font-semibold mb-1 py-2 pl-2"/>
+                                            <div className='inline-block dark:text-zinc-200 hover:text-black dark:hover:text-white px-1 pt-2'>Error, not a valid address.</div>
+                                        </>
+                                }
+                                {
+                                    postcodeInputStatus==="success" &&
+                                        <>
+                                            <CheckOutlined className="inline-block text-green-400 dark:text-green-600 pb-[6px] text-xs text-light font-semibold mb-1 py-2 pl-2"/>
+                                            <div className='inline-block dark:text-zinc-200 hover:text-black dark:hover:text-white px-1 pt-2'>{formattedAddress||"Valid"}</div>
+                                        </>
+                                }
+                                
                             </div>
-                        <input type='file' className="hidden" />
-                        </label>
+                        </div>
+                    </div>
+                    
+                    <div className='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+                        <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' onClick={() => navigate('/')} >Cancel</button>
+                        <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' onClick={() => submit()}>Update Information</button>
                     </div>
                 </div>
-
-                <div className='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-                <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' onClick={() => navigate('/')} >Cancel</button>
-                <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Create</button>
-                </div>
             </div>
-        </div>
         </SvgBackground>
         </Background>
     )
 }
 
-export default AccountSettings;
+export default AccountSettings
